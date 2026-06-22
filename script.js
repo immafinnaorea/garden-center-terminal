107ba8eee553d74abdc4d577b095851ee9e762eb
immafinnaorea <45086366+immafinnaorea@users.noreply.github.com>
2026-06-21 22:48:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -352,7 +352,7 @@ function newPrompt() {
   currentPrompt = $("promptText").textContent;
   $("sidebarPrompt").innerHTML = `<em>${esc(currentPrompt)}</em>`;
   setStatus("💧 watering prompt...");
-}
+
 }
 
 function usePrompt(prompt, category) {
